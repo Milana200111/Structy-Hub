ceabc64e7cdb9e5c6738d9729573714af42634ce
Milana200111 <milanaanitha@gmail.com>
2025-06-04 19:15:03 +0530

    Solved 'linked list find' (python3)

--- Linked-List-I/038-linked-list-find/linked-list-find.py
+++ Linked-List-I/038-linked-list-find/linked-list-find.py
@@ -4,9 +4,14 @@
 #     self.next = None
 
 def linked_list_find(head, target):
-  cur=head
-  while(cur!=None):
-    if(cur.val==target):
-      return True 
-    cur=cur.next
-  return False 
+  if head==None:
+    return False
+  if(head.val==target):
+    return True 
+  return linked_list_find(head.next,target)
+  # cur=head
+  # while(cur!=None):
+  #   if(cur.val==target):
+  #     return True 
+  #   cur=cur.next
+  # return False 
