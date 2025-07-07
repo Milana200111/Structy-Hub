87bc192c3ada3e8a2ca32cbd7de0ef3810bca9e2
Milana200111 <milanaanitha@gmail.com>
2025-07-07 20:45:54 +0530

    Solved 'tree min value' (python3)

--- Binary-Tree-I/057-tree-min-value/tree-min-value.py
+++ Binary-Tree-I/057-tree-min-value/tree-min-value.py
@@ -3,8 +3,27 @@
 #     self.val = val
 #     self.left = None
 #     self.right = None
+#---------recursive--------
+# def tree_min_value(root):
+#   if root is None:
+#     return float('inf')
+#   return min(root.val,min(tree_min_value(root.left),tree_min_value(root.right)))
 
+#--------iterative----------
 def tree_min_value(root):
-  if root is None:
-    return float('inf')
-  return min(root.val,min(tree_min_value(root.left),tree_min_value(root.right)))
+  minimum=float('inf')
+  stack=[root]
+  while(stack):
+    node=stack.pop()
+    minimum=min(minimum,node.val)
+    if(node.right):
+      stack.append(node.right)
+    if(node.left):
+      stack.append(node.left)
+  return minimum
+
+
+
+
+      
+  
