29b825ebf8eb640dac13f4a2478f41a0bd8e77de
Milana200111 <milanaanitha@gmail.com>
2025-07-07 20:16:41 +0530

    Solved 'tree includes' (python3)

--- Binary-Tree-I/056-tree-includes/tree-includes.py
+++ Binary-Tree-I/056-tree-includes/tree-includes.py
@@ -3,10 +3,24 @@
 #     self.val = val
 #     self.left = None
 #     self.right = None
-
+#-----------recursive-------------
+# def tree_includes(root, target):
+#   if(root is None):
+#     return False
+#   if(root.val==target):
+#     return True 
+#   return tree_includes(root.left,target) or tree_includes(root.right,target)
 def tree_includes(root, target):
   if(root is None):
-    return False
-  if(root.val==target):
-    return True 
-  return tree_includes(root.left,target) or tree_includes(root.right,target)
+    return False 
+  stack=[root]
+  while(stack):
+    node=stack.pop()
+    if(node.val==target):
+      return True 
+    if(node.right):
+      stack.append(node.right)
+    if(node.left):
+      stack.append(node.left)
+  return False
+      
