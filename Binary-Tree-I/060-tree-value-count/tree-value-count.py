b237881786941b1b80195f991178907c47ae8e46
Milana200111 <milanaanitha@gmail.com>
2025-07-08 15:44:51 +0530

    Solved 'tree value count' (python3)

--- Binary-Tree-I/060-tree-value-count/tree-value-count.py
+++ Binary-Tree-I/060-tree-value-count/tree-value-count.py
@@ -4,17 +4,32 @@
 #     self.left = None
 #     self.right = None
 
+#---------iterative--------
+# def tree_value_count(root, target):
+#   if root is None:
+#     return 0 
+#   count=0 
+#   stack=[root]
+#   while(stack):
+#     node=stack.pop()
+#     if(node.val==target):
+#       count+=1
+#     if(node.right):
+#       stack.append(node.right)
+#     if(node.left):
+#       stack.append(node.left)
+#   return count 
+  
+# #--------recursive--------
 def tree_value_count(root, target):
   if root is None:
     return 0 
-  count=0 
-  stack=[root]
-  while(stack):
-    node=stack.pop()
-    if(node.val==target):
-      count+=1
-    if(node.right):
-      stack.append(node.right)
-    if(node.left):
-      stack.append(node.left)
-  return count 
+  if root.val==target:
+    return 1+tree_value_count(root.left,target)+tree_value_count(root.right,target)
+  else:
+    return 0+tree_value_count(root.left,target)+tree_value_count(root.right,target)
+    
+
+
+
+
