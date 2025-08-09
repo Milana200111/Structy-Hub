b6e9c9405831e7347882fc81cb445688476fb638
Milana200111 <milanaanitha@gmail.com>
2025-08-09 23:27:05 +0530

    Solved 'can color' (python3)

---
+++ Graph-II/168-can-color/can-color.py
@@ -0,0 +1,34 @@
+from collections import deque 
+def can_color(graph):
+  colour_dict={}
+  for node in graph:
+    if node not in colour_dict:
+      if(not colour(node,graph,colour_dict)):
+        return False
+  return True 
+def colour(node,graph,colour_dict):
+  queue=deque([(node,'red')])
+  colour_dict[node]='red'
+  while(queue):
+    node,color=queue.popleft()
+    if(color=='red'):
+      neighbor_color='blue'
+    else:
+      neighbor_color='red'
+    for neighbor in graph[node]:
+      if neighbor not in colour_dict:
+        colour_dict[neighbor]=neighbor_color
+        queue.append((neighbor,neighbor_color))
+      else:
+        if(colour_dict[neighbor]!=neighbor_color):
+         return False 
+  return True 
+
+
+
+
+
+
+      
+      
+  
